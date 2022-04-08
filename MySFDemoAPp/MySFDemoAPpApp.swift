8efaa04a26aaedc8aa0f024e0ec5aa09cd1d6900
Michael Prenez-Isbell <prenezisbell@gmail.com>
2022-04-07 21:59:44 -0400

    Initial Commit

---
+++ MySFDemoAPp/MySFDemoAPpApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySFDemoAPpApp.swift
+//  MySFDemoAPp
+//
+//  Created by Michael Prenez-Isbell on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MySFDemoAPpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
